f30e96264f80d9a067fd83cf5a0ccf63ee2b86a4
elfbrtc <elif.barutcu.5353@hotmail.com>
2022-06-30 21:03:02 +0300

    Agenda sidebar created.

---
+++ src/components/homebody/Sidebar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    
+        
+          <div className='sidebar-tag'>
+            <div className='agenda-title'>
+              <h5>Gündem</h5>
+            </div>
+            <div className='hashtag'>
+              <ul>
+                <li>
+                  
+                  <h6>Diyarbakır</h6>
+                  <p>300 gönderi+</p>
+                  <h6>Tarsus</h6>
+                  <p>200 gönderi+</p>
+                  <h6>Rize</h6>
+                  <p>100 gönderi+</p>
+                </li>
+              </ul>
+
+            </div>
+            <div className='sidebar-button'>
+              <button>
+                Daha fazla göster
+              </button>
+              
+            </div>
+            
+          </div>
+        
+
+      
+  )
+}
+
+export default Sidebar
